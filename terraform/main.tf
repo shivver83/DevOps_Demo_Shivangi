a25ef4670b62fe382eecdf94ee49a30b25577e90
root <root@helm.icjxg5ncifsehhf1tzhq3q0jdb.dx.internal.cloudapp.net>
2020-02-07 09:41:04 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   version = "=1.38.0"
   subscription_id = "08613c68-8109-4ed0-9d14-050ed99c33cc"
   tenant_id       = "083b69ae-9b84-45ee-b8da-c4ba52a65031"
-  access_key      = "pzHVwVZnZtEj1GGZMbicnN0SYqQ97wI1TwR9pgoxrO9we/I8W/BJKzMfonzOwozYv+lW/BLYs1oWNWBQC4iGfA=="
+  access_key      = ""
 }
 
 resource "azuread_application" "TestAppDocker" {
